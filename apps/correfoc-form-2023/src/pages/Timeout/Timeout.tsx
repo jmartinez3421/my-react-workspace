88c653fd31c1aea460e0034ec586bbc7742fe796
Jordi Martinez <jmartinezg.fp@gmail.com>
2023-07-12 18:26:04 +0200

    fix(refresh the page if the counter reaches 0)#2

--- apps/correfoc-form-2023/src/pages/Timeout/Timeout.tsx
+++ apps/correfoc-form-2023/src/pages/Timeout/Timeout.tsx
@@ -5,6 +5,10 @@ import styles from "./Timeout.module.scss";
 export const Timeout = () => {
     const { days, hours, minutes, seconds } = useCountdown("2023-08-12T21:00:00");
 
+    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+        window.location.reload();
+    }
+
     return (
         <div className={styles.container}>
             <div>Estigues atent a l'enllaç, aviat tindràs noticies nostres.</div>
